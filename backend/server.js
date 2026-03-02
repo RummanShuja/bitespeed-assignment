b80690b1df6cfd94074b9591f9647566f3aa4630
RummanShuja <shujarumman@gmail.com>
2026-03-02 20:12:19 +0530

    add database connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,25 @@
+import dotenv from 'dotenv'
 import express from 'express'
+import mysql from 'mysql2/promise'
 
+dotenv.config();
 const app = express();
 const PORT = 3000;
 
+const db = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+db.connect((err)=>{
+    if(err){
+        console.log("DB connection failed: ", err );
+    }else{
+        console.log("Connected to Database");
+    }
+});
 
 app.get('/', (req, res)=>{
     res.send("Api working");
