bbe510d5b242774b8333e0dc961dfa9fdd8a30df
RummanShuja <shujarumman@gmail.com>
2026-03-03 00:51:43 +0530

    Handling secondary contact creation

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 app.use(express.json());
 let db;
 
-try{
+try {
     db = await mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
@@ -16,46 +16,45 @@ try{
         database: process.env.DB_DATABASE
     });
     console.log("Connected to Database");
-}catch(err){
-    console.log("DB connection failed: ", err );
+} catch (err) {
+    console.log("DB connection failed: ", err);
 }
 
 
 
-app.post('/identify', async(req, res)=>{
+app.post('/identify', async (req, res) => {
     // try catch
-    const {email, phoneNumber} = req.body;
-    console.log("email= ",email," phoneNumber= " ,phoneNumber);
-    if(!email && !phoneNumber){
-        return res.status(400).json({success:false, message:"You must enter either email or phoneNumber or both"});
+    const { email, phoneNumber } = req.body;
+    console.log("email= ", email, " phoneNumber= ", phoneNumber);
+    if (!email && !phoneNumber) {
+        return res.status(400).json({ success: false, message: "Either email or phoneNumber must be provided" });
     }
 
     const [rows] = await db.execute(`
             SELECT *
             FROM Contact 
             WHERE email=? OR phoneNumber=? ;    
-        `,[email || null, phoneNumber || null]);
-      
-    
-    if(rows.length === 0){
+        `, [email || null, phoneNumber || null]);
+
+
+    if (rows.length === 0) {
         // There are no existing contact against an incoming request
         const [result] = await db.execute(`
             INSERT INTO Contact (email, phoneNumber,linkPrecedence) VALUES
             (?,?,'primary')
-            `,[email || null, phoneNumber || null]);
-        
-        const newContactId = result.id;
+            `, [email || null, phoneNumber || null]);
+
+        const newContactId = result.insertId;
         const response = {
             "contact": {
-                "primaryContactId" : newContactId,
-                "emails" : email ? [email] : [],
+                "primaryContactId": newContactId,
+                "emails": email ? [email] : [],
                 "phoneNumbers": phoneNumber ? [phoneNumber] : [],
-                "secondaryContactIds": [] 
+                "secondaryContactIds": []
             }
         }
         return res.status(201).json(response);
     }
-
 
     const emailSet = new Set();
     const phoneNumberSet = new Set();
@@ -63,28 +62,28 @@ app.post('/identify', async(req, res)=>{
     let primaryContactId;
     let primaryEmail;
     let primaryPhoneNumber;
-    
-    for(let row of rows){
-        if(row.linkPrecedence=='primary'){
+
+    for (let row of rows) {
+        if (row.linkPrecedence == 'primary') {
             primaryContactId = row.id;
-            if(row.email) {
+            if (row.email) {
                 primaryEmail = row.email;
                 emailSet.add(row.email);
             }
-            if(row.phoneNumber){
+            if (row.phoneNumber) {
                 primaryPhoneNumber = row.phoneNumber;
                 phoneNumberSet.add(row.phoneNumber);
-            } 
+            }
         }
-        else{
+        else {
             primaryContactId = row.linkedId;
-            if(row.email) {
+            if (row.email) {
                 emailSet.add(row.email);
             }
-            if(row.phoneNumber){
+            if (row.phoneNumber) {
                 phoneNumberSet.add(row.phoneNumber);
-            } 
-            if(row.id) secondaryContactIdsSet.add(row.id);
+            }
+            if (row.id) secondaryContactIdsSet.add(row.id);
         }
     }
 
@@ -92,55 +91,89 @@ app.post('/identify', async(req, res)=>{
             SELECT * FROM Contact
             WHERE id = ? OR linkedId = ?;
         `, [primaryContactId || null, primaryContactId || null]);
-    
-    for(let row of linkedRows){
-        if(row.linkPrecedence=='primary'){
-            if(row.email) {
+
+    for (let row of linkedRows) {
+        if (row.linkPrecedence == 'primary') {
+            if (row.email) {
                 primaryEmail = row.email;
                 emailSet.add(row.email);
             }
-            if(row.phoneNumber){
+            if (row.phoneNumber) {
                 primaryPhoneNumber = row.phoneNumber;
                 phoneNumberSet.add(row.phoneNumber);
-            } 
+            }
         }
-        else{
-            if(row.email) emailSet.add(row.email);
-            if(row.phoneNumber) phoneNumberSet.add(row.phoneNumber);
-            if(row.id) secondaryContactIdsSet.add(row.id);
+        else {
+            if (row.email) emailSet.add(row.email);
+            if (row.phoneNumber) phoneNumberSet.add(row.phoneNumber);
+            if (row.id) secondaryContactIdsSet.add(row.id);
         }
 
     }
 
     let secondaryContactIds = [...secondaryContactIdsSet];
     let emails = [...emailSet];
-    // remove the primary email
-    if(primaryEmail) emails = emails.filter((item) => item!=primaryEmail);
-    // remove the primary phone number
-    let phoneNumbers = [...phoneNumberSet];
-    if(primaryPhoneNumber) phoneNumbers = phoneNumbers.filter((number)=> number!=primaryPhoneNumber); 
 
-    //add the primary phone number and email in the start
-    emails.unshift(primaryEmail);
-    phoneNumbers.unshift(primaryPhoneNumber);
-   
+    if (primaryEmail) {
+        emails = emails.filter((item) => item != primaryEmail);
+        emails.unshift(primaryEmail);
+    }
+    let phoneNumbers = [...phoneNumberSet];
+    if (primaryPhoneNumber){
+        phoneNumbers = phoneNumbers.filter((number) => number != primaryPhoneNumber);
+        phoneNumbers.unshift(primaryPhoneNumber);
+    } 
+
+
+    // an incoming request has either of phoneNumber or email common to an existing
+    // contact but contains new information
+    const emailExists = rows.some((row) => row.email === email);
+    const phoneNumberExists = rows.some((row) => row.phoneNumber === phoneNumber);
+
+    if ((email && !emailExists) || (phoneNumber && !phoneNumberExists)) {
+
+        const [result] = await db.execute(`
+                INSERT INTO Contact (email, phoneNumber,linkedId, linkPrecedence) VALUES
+                (?,?,?,'secondary');
+                `, [email, phoneNumber, primaryContactId]);
+        const newContactId = result.insertId;
+        
+        if (phoneNumber && !phoneNumberExists) {
+            phoneNumbers.push(phoneNumber);
+        }
+        if(email && !emailExists){
+            emails.push(email);
+        }
+
+        secondaryContactIds.push(newContactId);
+
+        const response = {
+            "contact": {
+                "primaryContactId": primaryContactId,
+                "emails": emails,
+                "phoneNumbers": phoneNumbers,
+                "secondaryContactIds": secondaryContactIds
+            }
+        }
+        return res.status(201).json(response);
+    }
 
     const response = {
         "contact": {
-            "primaryContactId" : primaryContactId,
+            "primaryContactId": primaryContactId,
             "emails": emails,
             "phoneNumbers": phoneNumbers,
-            "secondaryContactIds": secondaryContactIds 
+            "secondaryContactIds": secondaryContactIds
         }
     }
 
-    res.status(200).send(response);
+    res.status(200).json(response);
 })
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Api working");
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is listening to port: ${PORT}`);
 })
